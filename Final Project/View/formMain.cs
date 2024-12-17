c074d54406b37f846ed444171c48232c6a42de3f
affan zuko <affanzuko18@gmail.com>
2024-12-17 09:35:39 +0700

    sudha work

--- Final Project/View/formMain.cs
+++ Final Project/View/formMain.cs
@@ -161,19 +161,12 @@ namespace Final_Project
         private void btnKeranjang_Click(object sender, EventArgs e)
         {
             pnWelcome.Visible = false;
-            if (keranjang == null)
-            {
                 keranjang = new formKeranjang();
                 keranjang.FormClosed += keranjang_FormClosed;
                 keranjang.MdiParent = this;
                 keranjang.Dock = DockStyle.Fill;
                 keranjang.Show();
             }
-            else
-            {
-                keranjang.Activate();
-            }
-        }
 
         private void keranjang_FormClosed(object sender, FormClosedEventArgs e)
         {
